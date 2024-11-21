c51f4b5f1c16d973e25378bfce75c5111c5e59c9
SajadBhat <bsajad2@gmail.com>
2024-11-21 13:45:07 +0530

    Fix tqdm error

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
- 
+# Model configurations
+MODEL_CONFIG = {
+    'input_size': 784,
+    'hidden_size': 500,
+    'num_classes': 10,
+}
+
+# Training configurations
+TRAIN_CONFIG = {
+    'learning_rate': 0.001,
+    'batch_size': 64,
+    'num_epochs': 10,
+} 
